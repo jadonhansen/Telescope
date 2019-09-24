9946f4c6ac7abe3e160b524e0032ca744c6befe4
jadonhansen <jadongavhansen@gmail.com>
2019-09-24 13:18:52 +0200

    done header design 💡

--- srcs/options.c
+++ srcs/options.c
@@ -1,6 +1,23 @@
 #include "../includes/telescope.h"
 
+void	print_header()
+{
+	ft_putstr_colour(GREEN, "============================================\n");
+	ft_putstr_colour(GREEN, "/\\                                        /\\\n");
+	ft_putstr_colour(GREEN, "||                                        ||\n||");
+	ft_putstr_colour(BLUE, "               TELESCOPE©               ");
+	ft_putstr_colour(GREEN, "||\n||");
+	ft_putstr_colour(BLUE, "          A light in the dark 💡         ");
+	ft_putstr_colour(GREEN, "||\n||                                        ||\n");
+	ft_putstr_colour(GREEN, "||                                        ||\n");
+	ft_putstr_colour(GREEN, "============================================\n\n");
+}
+
 void	options(t_line **book)
 {
-	
+	// int	parameter = 0;
+	// int	instances = 0;
+	*book = NULL;
+	print_header();
+
 }
